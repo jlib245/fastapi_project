9be1533f9ccb04bc8b4fb88f592459653858479f
jlib245 <jlib245@naver.com>
2025-03-17 17:26:19 +0900

    DB 연결

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -7,7 +7,7 @@ from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from src.app import database # models.py 파일 import
+from src.app import models # models.py 파일 import
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -23,7 +23,7 @@ if config.config_file_name is not None:
 
 # target_metadata = mymodel.Base.metadata
 #target_metadata = None
-target_metadata = database.Base.metadata # models.py 파일의 Base.metadata를 target_metadata로 설정
+target_metadata = models.Base.metadata # models.py 파일의 Base.metadata를 target_metadata로 설정
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- backend/src/app/models/answer.py
+++ backend/src/app/models/answer.py
@@ -6,4 +6,4 @@ class Answer(Base):
     content = Column(Text, nullable=False)
     create_date = Column(DateTime, nullable=False)
     question_id = Column(Integer, ForeignKey("questions.id")) # ForeignKey(참조할 테이블.참조할 컬럼 {외부 키}) : 모델 연결
-    question = relationship("Question", backref="answers") # Question 모델과 연결, 역참조조
+    question = relationship("Question", backref="answers") # Question 모델과 연결, 역참조
